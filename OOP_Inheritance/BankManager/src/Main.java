77ecb8c72675ae8d57a955bf064ea0783d47bee1
Karolina Smentoch <karolinasmentoch@gmail.com>
2023-01-02 22:49:43 +0100

    Lesson Inheritance - first part

---
+++ OOP_Inheritance/BankManager/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        CheckingAccount checkingAccount = new CheckingAccount();
+        SavingsAccount savingsAccount = new SavingsAccount();
+        savingsAccount.balance = 10000;
+        checkingAccount.account = 400;
+
+    }
+
+}
